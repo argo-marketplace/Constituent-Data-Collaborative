6f7b929faca67301fabce0f380afe9f425e9f0b1
Varun Adibhatla <vr00n@users.noreply.github.com>
2019-03-21 08:01:16 -0400

    Update census_api_cds_script.sh

--- scripts/census_api_cds_script.sh
+++ scripts/census_api_cds_script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-for input in $(cat cds.txt)
+for input in $(cat cds.txt) # these are codes of type 01 02 where 01 = State Code and 22 = Congressional District Code
 do
- for var in $(cat vars.txt)
+ for var in $(cat vars.txt) # choose the variables you want to poll. A full list of variables is available at https://api.census.gov/data/2017/acs/acs5/profile/variables.json
   do
 	state=`echo $input | awk -F~ '{print $1}'`
 	cd=`echo $input | awk -F~ '{print $2}'`
 	echo state,$state,cd,$cd,var,$var
 	value=`curl -s "https://api.census.gov/data/2017/acs/acs5/profile?key=fd901c69fb4729a262b7e163c1db69737513827d&get=$var&for=congressional%20district%3A$cd&in=state%3A$state" | jq .[1][0] | tr -d \"`
-	echo $state,$cd,$var,$value >>result.txt
-	#sleep 1
+	##This script outputs to a result.txt file
+	echo $state,$cd,$var,$value >>result.txt 
   done
 done
